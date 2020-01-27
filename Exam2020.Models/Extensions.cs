919a04e4ffd3521e980509638365a44422fda774
Helmoz <helmo.designer1@gmail.com>
2020-01-27 14:29:03 +0300

    fix

---
+++ Exam2020.Models/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace Exam2020.Models
+{
+    public static class StringExtensions
+    {
+        public static string GetHost(this string url)
+        {
+            if (!url.IsValidUrl())
+            {
+                return "";
+            }
+
+            return new Uri(url).Host;
+        }
+
+        public static bool IsValidUrl(this string url)
+        {
+            var pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+            var rgx = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return rgx.IsMatch(url);
+        }
+    }
+
+    public static class HtmlNodeExtensions
+    {
+        public static string GetHref(this HtmlNode htmlNode)
+        {
+            return htmlNode.Attributes["href"].Value;
+        }
+    }
+}
---
+++ Exam2020.Models/UrlContent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exam2020.Models
+{
+    public class UrlContent
+    {
+        public long Id { get; set; }
+
+        public string Url { get; set; }
+
+        public string Body { get; set; }
+
+        public UrlContent(string url, string body)
+        {
+            Url = url;
+            Body = body;
+        }
+
+    }
+}
